f36de170fbc2b037c9ff50f6c1c61d7e6c0c7423
Marck <marperez@gmail.com>
2022-10-05 23:53:27 +0000

    Adding azure version

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
 def toyou(x):
     return f"hi {x}"
 
-def add(x):
-    return x + 1
+def add(x, y):
+    return x + y
 
 def subtract(x):
     return x - 1
+
+#var=
+result = add(1,2)
+print(f"This is the sum: 1, 2, {result}")
+
     
